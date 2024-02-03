665004b95861e162e3e342116d8f2c5c0e254823
Nikita Blokhin <blokhinnikita@gmail.com>
2024-02-03 10:59:50 +0200

    version for review

--- the_snake.py
+++ the_snake.py
@@ -1,4 +1,4 @@
-"""Импорт модулей, которые будут использованы при написании кода."""
+"""Импорт модулей, использованных в коде."""
 from random import choice, randint
 
 import pygame
@@ -48,12 +48,12 @@ class GameObject:
     """Родительский класс игровых объектов."""
 
     def __init__(self, body_color=None):
-        """Конструктор класса,инициализирующий игровые объекты."""
+        """Конструктор родительского класса, создающий игровые объекты."""
         self.position = ((SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2))
         self.body_color = body_color
 
     def draw(self, surface):
-        """Метод для отрисовки игровых объектов. Пэсс по умолчанию,тк переопределяется в дочерних классах."""
+        """Метод для отрисовки игровых объектов."""
         pass
 
 
@@ -61,7 +61,7 @@ class Apple(GameObject):
     """Дочерний класс, описывающий яблоко."""
 
     def __init__(self, body_color=APPLE_COLOR):
-        """Конструктор дочернего класса, инициализирующий яблоко."""
+        """Конструктор дочернего класса, создающий яблоко."""
         super().__init__(body_color)
         self.randomize_position()
 
@@ -83,7 +83,7 @@ class Snake (GameObject):
     """Дочерний класс, описывающий змейку."""
 
     def __init__(self, body_color=SNAKE_COLOR):
-        """Конструктор дочернего класса, инициализирующий змейку."""
+        """Конструктор дочернего класса, создающий змейку."""
         super().__init__(body_color)
         self.length = 1
         self.positions = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
@@ -93,7 +93,7 @@ class Snake (GameObject):
         self.last = None
 
     def update_direction(self):
-        """Метод, обновляющий напрвления движения змейки."""
+        """Метод, обновляющий направления движения змейки."""
         if self.next_direction:
             self.direction = self.next_direction
             self.next_direction = None
@@ -106,9 +106,11 @@ class Snake (GameObject):
         """Метод, обновляющий позицию змейки."""
         actual_position = self.get_head_position()
         new_position_x = (
-            actual_position[0] + self.direction[0] * GRID_SIZE) % SCREEN_WIDTH
+            actual_position[0] + self.direction[0]
+            * GRID_SIZE) % SCREEN_WIDTH
         new_position_y = (
-            actual_position[1] + self.direction[1] * GRID_SIZE) % SCREEN_HEIGHT
+            actual_position[1] + self.direction[1]
+            * GRID_SIZE) % SCREEN_HEIGHT
         new_head_position = (new_position_x, new_position_y)
         self.positions.insert(0, new_head_position)
 
@@ -126,15 +128,17 @@ class Snake (GameObject):
         return True
 
     def draw(self, surface):
-        """Метод для отрисовки змейкию."""
-        for position in self.positions[:-1]:
-            rect = (
-                pygame.Rect((position[0], position[1]), (GRID_SIZE, GRID_SIZE))
-            )
+        """Метод для отрисовки змейки."""
+        for position in self.positions:
+            rect = pygame.Rect(
+                (position[0], position[1]),
+                (GRID_SIZE, GRID_SIZE))
+
             pygame.draw.rect(surface, self.body_color, rect)
             pygame.draw.rect(surface, BORDER_COLOR, rect, 1)
 
-        head_rect = pygame.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
+        head_rect = pygame.Rect(self.positions[0],
+                                (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(surface, self.body_color, head_rect)
         pygame.draw.rect(surface, BORDER_COLOR, head_rect, 1)
 
@@ -146,7 +150,7 @@ class Snake (GameObject):
             pygame.draw.rect(surface, BOARD_BACKGROUND_COLOR, last_rect)
 
     def reset(self):
-        """Метод для сброса змейки до первоначального состояния при возобновлении игры."""
+        """Метод для сброса змейки при возобновлении игры."""
         self.length = 1
         self.positions = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
         self.direction = choice([RIGHT, LEFT, UP, DOWN])
